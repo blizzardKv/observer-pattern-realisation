5e73957e9e9a00362d7e98ae859fe36349dcb23f
blizzardKv <makemewanna@yandex.ru>
2021-08-05 21:25:27 +0300

    observer pattern realisation

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let data = { price: 5, quantity: 2};
+let target, total, salePrice;
+
+class Dep {
+  constructor() {
+    this.subscribers = [];
+  }
+
+  depend() {
+    if (target && !this.subscribers.includes(target)) {
+      this.subscribers.push(target);
+    }
+  }
+
+  notify() {
+    this.subscribers.forEach(sub => sub());
+  }
+}
+
+Object.keys(data).forEach(key => {
+  let internalValue = data[key];
+
+  const dep = new Dep();
+
+  Object.defineProperty(data, key, {
+    get() {
+      dep.depend();
+      return internalValue;
+    },
+    set(newVal) {
+      internalValue = newVal;
+      dep.notify();
+    }
+  });
+});
+
+function watcher(myFunc) {
+  target = myFunc;
+  target();
+  target = null;
+}
+
+watcher(() => {
+  total = data.price * data.quantity;
+});
+
+watcher(() => {
+  salePrice = data.price * 0.9;
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  data.price += 20;
+  document.querySelector('a').innerText = data.price;
+});
